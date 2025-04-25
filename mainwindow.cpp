4c20bdba9fbc0969b6452747bd30527d883992fd
袁江峰 <jiangfeng.yuan@yo-star.com>
2025-04-25 15:58:01 +0800

    feat: build package

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,20 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QObject>
+#include <QThread>
 #include <QString>
+#include <QTimeLine>
+#include <QMessageBox>
+#include <iostream>
+#include "crc.h"
+
+std::string Worker::doWork()
+{
+  CRC calculatorCRC64(this->crc_);
+  std::string result = calculatorCRC64.file(this->path_).dec();
+  emit Worker::workFinished(result);
+  return result;
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
@@ -8,7 +22,14 @@ MainWindow::MainWindow(QWidget *parent)
   ui->setupUi(this);
   this->statusBar()->setSizeGripEnabled(false);
 
+  std::vector<std::string> list = CRC::getPoly();
+  for (auto item : list)
+  {
+    ui->selectCRC->addItem(QString::fromStdString(item));
+  }
+
   connect(ui->labelPath, &ClickableLabel::clicked, this, &MainWindow::openFileDialog);
+  connect(ui->btnCalculate, &QPushButton::clicked, this, &MainWindow::calculateCrc);
 }
 
 MainWindow::~MainWindow()
@@ -31,12 +52,62 @@ void MainWindow::openFileDialog()
   }
 }
 
-std::string MainWindow::calculateCrc()
+void MainWindow::updateHistoryLabel(std::string p, std::string res)
 {
-  // CRC calculatorCRC64("crc-64-ECMA");
+  this->history_.push_back({p, res});
+  std::string historyRes = "";
+  for (size_t i = 0; i < this->history_.size(); i++)
+  {
+    const auto &[path, result] = this->history_[i];
+    historyRes += std::to_string(i + 1) + ".  " + path + "\t" + result + "\n";
+  }
 
-  // std::string result = calculatorCRC64.file(this->filePath_).dec();
-  // this->ui->labelResult->setText(QString::fromStdString(result));
-  // return result;
-  return "";
+  ui->labelHistory->setText(QString::fromStdString(historyRes));
+}
+
+void MainWindow::calculateCrc()
+{
+  if (this->filePath_.empty())
+  {
+    QMessageBox::critical(this, "计算错误", "请先选择文件！");
+    return;
+  }
+  if (ui->selectCRC->currentText().isEmpty())
+  {
+    QMessageBox::critical(this, "计算错误", "请先选择CRC算法！");
+    return;
+  }
+  ui->btnCalculate->setEnabled(false);
+  auto userSelectedCrc = ui->selectCRC->currentText();
+
+  QTimeLine *timeLine = new QTimeLine(1000, this);
+  timeLine->setFrameRange(0, 4);
+  timeLine->setLoopCount(0);
+  connect(timeLine, &QTimeLine::frameChanged, this, [this](int frame)
+          {
+    QString text = "calculating" + QString(".").repeated(frame);
+    ui->labelResult->setText(text); });
+
+  QThread *thread = new QThread;
+  Worker *worker = new Worker(userSelectedCrc.toStdString(), this->filePath_);
+  worker->moveToThread(thread);
+
+  connect(thread, &QThread::started, worker, &Worker::doWork);
+  connect(worker, &Worker::workFinished, this, [this, timeLine](std::string result)
+          {
+            timeLine->stop();
+            this->handleCrcResult(result);
+            this->ui->btnCalculate->setEnabled(true); });
+  connect(worker, &Worker::workFinished, worker, &QObject::deleteLater);
+  connect(thread, &QThread::finished, worker, &Worker::deleteLater);
+  connect(thread, &QThread::finished, thread, &QThread::deleteLater);
+
+  timeLine->start();
+  thread->start();
+}
+
+void MainWindow::handleCrcResult(std::string result)
+{
+  this->ui->labelResult->setText(QString::fromStdString(result));
+  this->updateHistoryLabel(this->filePath_, result);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,53 +3,75 @@
 
 #include <QFileDialog>
 #include <QLabel>
+#include <QObject>
 #include <QMainWindow>
 #include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-class ClickableLabel;
+namespace Ui
+{
+  class MainWindow;
+  class ClickableLabel;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+// 工作线程类
+class Worker : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
+private:
+  std::string crc_;
+  std::string path_;
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  Worker(std::string crc, std::string path) : crc_(crc), path_(path) {};
+public slots:
+  std::string doWork();
+signals:
+  void workFinished(std::string);
+};
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    std::string filePath_;
+  Ui::MainWindow *ui;
+  std::string filePath_;
+  std::vector<std::pair<std::string, std::string>> history_;
 
 private slots:
-    void openFileDialog();
-    std::string calculateCrc();
+  void openFileDialog();
+  void updateHistoryLabel(std::string p, std::string res);
+  void calculateCrc();
+  void handleCrcResult(std::string result);
 };
 
 class ClickableLabel : public QLabel
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit ClickableLabel(QWidget *parent = nullptr)
-        : QLabel(parent)
-    {
-        setMouseTracking(true);
-    }
+  explicit ClickableLabel(QWidget *parent = nullptr)
+      : QLabel(parent)
+  {
+    setMouseTracking(true);
+  }
 
 signals:
-    void clicked();
+  void clicked();
 
 protected:
-    void mousePressEvent(QMouseEvent *event) override
+  void mousePressEvent(QMouseEvent *event) override
+  {
+    if (event->button() == Qt::LeftButton)
     {
-        if (event->button() == Qt::LeftButton) {
-            emit clicked();
-        }
-        QLabel::mousePressEvent(event);
+      emit clicked();
     }
+    QLabel::mousePressEvent(event);
+  }
 };
 #endif // MAINWINDOW_H
